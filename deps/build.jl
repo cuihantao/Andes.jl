7f0f9abbee4d6a9dd4e744a1eed533ed99829476
Hantao Cui <hcui7@utk.edu>
2020-04-03 22:06:06 -0400

    added the uuid of Pkg from the repo:

--- deps/build.jl
+++ deps/build.jl
@@ -1,3 +1,15 @@
 using Pkg
 using PyCall
 using Conda
+
+try
+    pyimport("andes")
+catch
+    @warn "PyCall is not configured to an existing Python env."
+    @warn "andes.jl will use Conda for PyCall and install andes."
+    ENV["PYTHON"] = Conda.PYTHONDIR
+    Pkg.build("PyCall")
+
+    Conda.add("andes", channel="conda-forge")
+end
+
