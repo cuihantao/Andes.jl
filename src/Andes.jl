71ec0dae8df0c8eed80abb3f46e353099f9f499e
Hantao Cui <cuihantao@gmail.com>
2021-10-27 17:01:01 -0500

    Include KVXOPT converters.

--- src/Andes.jl
+++ src/Andes.jl
@@ -1,13 +1,21 @@
 __precompile__()
 
 module Andes
+
 using Conda
 using PyCall
+
+import SparseArrays
 
 const py = PyNULL()
 
 function __init__()
     copy!(py, pyimport_conda("andes", "andes", "conda-forge"))
 end
+
+include("kvxopt.jl")
+
+# --- export ---
+export convert;
 
 end # module
